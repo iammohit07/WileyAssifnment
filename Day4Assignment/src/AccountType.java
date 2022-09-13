6750d29d9366ade3cea5a0285104b4fdb1fde33b
Mohit Jangid <1165295mj@gmail.com>
2022-09-13 11:33:26 +0530

    Day 4 Assignment

---
+++ Day4Assignment/src/AccountType.java
@@ -0,0 +1,18 @@
+public enum AccountType {
+	Savings(500), Current(1000);
+
+	private double minBalance;
+
+	AccountType(double amount) {
+		this.minBalance = amount;
+	}
+
+	public boolean checkMinBal(double amount) {
+		if (this.minBalance > amount) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
